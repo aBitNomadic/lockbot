6a68dba7d256c55f844412c0d373700041314e2b
unknown <aBitNomadic01@gmail.com>
2018-04-17 23:15:33 -0400

    Added some commands login needs work, Started setText #BrokenBuild

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ import { CrudUser } from './users/crudUser';
 const client = new Discord.Client();
 
 createConnection().then(connection => {
-    console.log('Connected! '+ connection);
+  console.log('Connected to database database...');
 }).catch(error => console.log(error));
 
 client.on("ready", () => {
@@ -15,52 +15,52 @@ client.on("ready", () => {
 });
 
 //Remake code in Typescrpit formatting!
-client.on("message", (message) => {
-  let username = message.author.username;
-  if (message.content.startsWith("!login")){
-  //   if(mongo.isAdmin(user)){
-  //     admins.push(user);
-  //     message.reply(user + " has logged in.");
-  //     console.log(user + " has logged in.");
-  //   }
+client.on("message", (messageIn) => {
+let message = messageIn;
+let username = message.author.username;
+if (message.content.startsWith("!login")){
+  let findAdmin = new CrudUser();
+  findAdmin.isAdmin(username).then((result) => {
+    message.reply("you are a admin")
+  }).catch(error => console.log(error));
 } else if (message.content.startsWith("!addMe")){
   let createUser = new CrudUser();
-  createUser.addNewUser(username).then(
+  createUser.addNewUser(username).then((result) => {
     message.reply("you have been added to the database!")
-  ).catch(error => console.log(error));
-}
-  // else if (message.content.startsWith("!setText")){
-  //   if(admins.indexOf(user) != -1){
-  //     var smsg = message.content.split(" ", 2);
-  //     var kword = smsg[1];
-  //     var stmt = smsg.slice(nthIndex(smsg, " ", 3));
-  //     if(kword && stmt != -1){
-  //       mongo.addStatment(kword, stmt);
-  //       console.log('New statment added to mongo: '+ kword + ': ' + stmt);
-  //       message.reply('New statment added to mongo: '+ kword + ': ' + stmt);
-  //     } else {
-  //       message.reply('There was an issue in processing your new command')
-  //         .then();
-  //     }
+  }).catch(error => console.log(error));
+} else if (message.content.startsWith("!setText")){
+  let findAdmin = new CrudUser();
+  await findAdmin.isAdmin(username).then((result) => {
+  }).catch(error => console.log(error));
+  // if(admins.indexOf(user) != -1){
+  //   var smsg = message.content.split(" ", 2);
+  //   var kword = smsg[1];
+  //   var stmt = smsg.slice(nthIndex(smsg, " ", 3));
+  //   if(kword && stmt != -1){
+  //     mongo.addStatment(kword, stmt);
+  //     console.log('New statment added to mongo: '+ kword + ': ' + stmt);
+  //     message.reply('New statment added to mongo: '+ kword + ': ' + stmt);
   //   } else {
-  //     message.reply('you are not a logged in admin.')
-  //       .then(msg => console.log(msg.author.username + ' was not a logged in admin.'))
-  //       .catch(console.err);
+  //     message.reply('There was an issue in processing your new command')
+  //     .then();
   //   }
-  //
+  // } else {
+  //   message.reply('you are not a logged in admin.')
+  //   .then(msg => console.log(msg.author.username + ' was not a logged in admin.'))
+  //   .catch(console.err);
   // }
-  else if (message.content.startsWith("ping")) {
-    message.reply("pong!");
-  }
-});
+
+} else if (message.content.startsWith("ping")) {
+  message.reply("pong!");
+}});
 
 client.login(botToken);
 //StackOverflow fo life1
 function nthIndex(str, pat, n){
-  var len = str.length, i= -1;
-  while(n-- && i++ < len){
-    i = str.indexOf(pat, i);
-    if (i < 0) break;
-  }
-  return i;
+var len = str.length, i= -1;
+while(n-- && i++ < len){
+  i = str.indexOf(pat, i);
+  if (i < 0) break;
+}
+return i;
 }
--- src/users/crudUser.ts
+++ src/users/crudUser.ts
@@ -3,15 +3,24 @@ import { User } from '../orm/entity/user';
 
 
 export class CrudUser {
-  constructor(){};
+  static entityManager: any;
+  constructor(){
+    CrudUser.entityManager = getManager();
+  };
 
   async addNewUser(username: string): Promise<any>{
-    const entityManager = getManager();
-    let user = await entityManager.create(User);
+    let user = await CrudUser.entityManager.create(User);
     user.username = username;
     user.isAdmin = true;
     user.joindate = new Date();
-    return await entityManager.save(user);
+    return await CrudUser.entityManager.save(user);
+  }
+
+  async isAdmin(username: string): Promise<any>{
+    let user = await CrudUser.entityManager.createQueryBuilder(User, "user")
+                                  .where("username = :inName", { inName: username })
+                                  .getOne();
+    console.log(user);
   }
 
 
